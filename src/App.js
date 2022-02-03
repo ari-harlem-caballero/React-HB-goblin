d5ec0f6b1b37306d30b3e34831a93dd0c172b047
ari-harlemcaballero <ari.bash.harlem@gmail.com>
2022-02-02 17:57:31 -0800

    created new goblin obj

--- src/App.js
+++ src/App.js
@@ -23,7 +23,12 @@ function App() {
     e.preventDefault()
     
     // on submit, make a new goblin object with a random id, a name that comes from the form state, an hp that comes from the form state, and a color that comes from the form state
-
+    const newGoblin = {
+      id: Math.ceil(Math.random() * 9999),
+      name: {goblinFormName},
+      hp: {goblinFormHP},
+      color: {goblinFormColor}
+    };
     // update the allGoblins array. Add the new goblin to the allGoblins array immutably.
     
     // clear out the goblin form state items by setting them to empty strings. This will cause the form to reset in the UI.
